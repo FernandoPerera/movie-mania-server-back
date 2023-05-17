8911cc502fd88bb7ded815212ba6074a6cdc30d6
Fernando Perera <fernando02perera@gmail.com>
2023-05-17 15:37:27 +0200

    first commit

---
+++ src/test/java/com/spike/moviemania/SpikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spike.moviemania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
